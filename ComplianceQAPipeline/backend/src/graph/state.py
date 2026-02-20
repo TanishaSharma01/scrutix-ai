c068bbbb7af777686f5900956fd7622bc2129a1f
Tanisha Sharma <tanishaofficial30@gmail.com>
2026-02-20 23:58:41 +1100

    designing schema

---
+++ ComplianceQAPipeline/backend/src/graph/state.py
@@ -0,0 +1,36 @@
+import operator
+from typing import Annotated, List, Dict, Optional, Any, TypedDict
+
+# define the schema for a single complicance result
+# Error Report
+class ComplianceIssue(TypedDict):
+    category: str # audio, video, text
+    description: str # specific detail of violation
+    severity: str # CRITICAL | WARNING
+    timestamp: Optional[str] 
+
+# define the global graph state
+# this defines the state that gets passed around in the agentic workflow
+class VideoAuditState:
+    '''
+    defines the data schema for langgraph execution content
+    '''
+    video_url: str
+    video_id: str
+
+    # ingestion and extraction data
+    local_file_path: Optional[str]
+    video_metadata: Dict[str, Any] # {"duration": 15, "resolution": "1080p"}
+    transcript: Optional[str] # Fully extracted speech-to-text
+    ocr_text: List[str] # text that appears at a screen cap
+
+    # analysis output
+    compliance_results: Annotated[List[ComplianceIssue], operator.add]
+
+    # final deliverables
+    final_status: str # PASS | FAIL
+    final_report: str # markdown format
+
+    # system observability
+    # errors: API timeout, system level errors
+    errors: Annotated[List[str], operator.add]
